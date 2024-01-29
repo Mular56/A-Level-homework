8a2f0390f440d370bc0bad05627ad4a4309175b6
Mular56 <90186293+Mular56@users.noreply.github.com>
2024-01-29 21:30:56 +0200

    Add files via upload

---
+++ module01/ModuleWork/first_test.py
@@ -0,0 +1,33 @@
+def spam(number):
+    
+    return ''.join(['bulochka' for i in range(number)])
+
+def my_sum(list_of_numbers):
+    
+    total_sum = 0
+    for number in list_of_numbers:
+        total_sum += number
+    return total_sum
+
+
+def shortener(string):
+    
+    string_split = string.split()
+    new_list = []
+    for word in string_split:
+        if len(word) > 6:
+            new_word = word[:6] + '*'
+        else:
+            new_word = word
+        new_list.append(new_word)
+    new_string = ' '.join(new_list)
+    return new_string
+
+
+def compare_ends(words):
+
+    words_count = 0
+    for word in words:
+        if len(word) >= 2 and word[0] == word[-1]:
+            words_count += 1
+    return words_count
---
+++ module01/long_term_event.py
@@ -0,0 +1,46 @@
+# долговременные события
+
+long_term_event = [
+    {
+        "title" : "piano",
+        "description": "learning to play the piano",
+        "price": 15,
+        "mood": 3,
+        "time": -6
+    },
+    {
+        "title" : "book",
+        "description": "writing a book",
+        "price": 0,
+        "mood": 4,
+        "time": -3
+    },
+    {
+        "title" : "prison",
+        "description": "is in prison(",
+        "price": 0,
+        "mood": -5,
+        "time": -5
+    },
+    {
+        "title" : "football",
+        "description": "sport",
+        "price": 50,
+        "mood": 6,
+        "time": -14
+    },
+
+    {
+        "title" : "pool",
+        "description": "hobby",
+        "price": 35,
+        "mood": 7,
+        "time": -16
+    },
+    {
+        "title" : "haircut",
+        "description": "beaty",
+        "price": 10,
+        "mood": 8,
+        "time": -14
+    }]
